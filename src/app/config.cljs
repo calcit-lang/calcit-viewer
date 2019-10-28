7b797fd54168409532b53a9c4802b1933eb955fa
jiyinyiyong <jiyinyiyong@gmail.com>
2019-10-29 01:07:11 +0800

    update workflow

--- src/app/config.cljs
+++ src/app/config.cljs
@@ -18,8 +18,6 @@
   {:dev-ui "http://localhost:8100/main.css",
    :release-ui "http://cdn.tiye.me/favored-fonts/main.css",
    :cdn-url "http://cdn.tiye.me/calcit-viewer/",
-   :cdn-folder "tiye.me:cdn/calcit-viewer",
    :title "Calcit Viewer",
    :icon "http://cdn.tiye.me/logo/cirru.png",
-   :storage-key "calcit-viewer",
-   :upload-folder "tiye.me:repo/Cirru/calcit-viewer/"})
+   :storage-key "calcit-viewer"})
